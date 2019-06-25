eab9b3feb0f5e7cfb132690add65507ef9f5060b
hadoop <vishnoiprem@gmail.com>
2019-06-25 22:52:42 +0800

    feature/1

--- 00-framework-tool-introductions/01.Eclipse-Introduction-in-5-steps/src/HelloWorld.java
+++ 00-framework-tool-introductions/01.Eclipse-Introduction-in-5-steps/src/HelloWorld.java
@@ -1,9 +1,22 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("key", "value");
+        map.put("key", "value");
+
+        System.out.print(map);
+
+        Map <String, Long> maplong= new HashMap<String, Long>();
+        maplong.put("1", (long) 10);
+        System.out.print(maplong);
+
+        Person person=new Person("prem","vis","1p");
+        System.out.print(person);
+        System.out.print(person.hashCode());
+
 	}
 }
